1c698f89cf12aa69fc0d376a51d6fa2ced9d3eca
Ramez Taher <Rameztaher2002@gmail.com>
2022-07-27 15:29:47 +0100

    fetch products to home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import products from "../products"
+import { Row, Col } from "react-bootstrap"
+import Product from "../components/Product"
+
+const Home = () => {
+  return (
+    <>
+      <h1>The Lastest Products</h1>
+      <Row>
+        {products.map((product, idx) => (
+          <Col key={idx} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
+
+export default Home
