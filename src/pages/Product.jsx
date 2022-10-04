cddc4356c459e524ddbf1e7b25619507f258a553
Ramez Taher <ramezgamer11@gmail.com>
2022-10-04 22:12:56 +0100

    Handling Quantity buying

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,23 +1,25 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { Link, useParams } from "react-router-dom"
-import { Col, Row, Image, ListGroup, Card, Button } from "react-bootstrap"
+import { Col, Row, Image, ListGroup, Card, Button, Form } from "react-bootstrap"
 import Rating from "../components/Rating"
 import { useDispatch, useSelector } from "react-redux"
 import { product } from "../actions/productActions"
 import Loader from "../components/Loader"
 import Message from "../components/Message"
 const Product = () => {
+  let { id } = useParams()
+  const [quantity, setQuantity] = useState(0)
   const dispatch = useDispatch()
 
-  let { id } = useParams()
-
   const productDetails = useSelector((state) => state.productDetails)
-
   const { loading, error, data } = productDetails
 
   useEffect(() => {
     dispatch(product(id))
   }, [dispatch, id])
+
+  const addToCart = () => {}
+
   return (
     <>
       <Link className="btn btn-dark my-3" to={"/"}>
@@ -31,10 +33,10 @@ const Product = () => {
         <Message variant="danger">productsList.error</Message>
       ) : (
         <Row>
-          <Col md={6}>
+          <Col lg={6}>
             <Image src={data?.image} alt={data?.name} fluid />
           </Col>
-          <Col md={3}>
+          <Col lg={3}>
             <ListGroup variant="flush">
               <ListGroup.Item>
                 <h2>{data?.name}</h2>
@@ -49,7 +51,7 @@ const Product = () => {
               <ListGroup.Item>Description: ${data?.description}</ListGroup.Item>
             </ListGroup>
           </Col>
-          <Col md={3}>
+          <Col lg={3}>
             <Card>
               <ListGroup variant="flush">
                 <ListGroup.Item>
@@ -68,8 +70,31 @@ const Product = () => {
                     </Col>
                   </Row>
                 </ListGroup.Item>
+
+                {data?.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Quantity</Col>
+                      <Col>
+                        <Form.Control
+                          as="select"
+                          value={quantity}
+                          onChange={(e) => setQuantity(e.target.value)}
+                        >
+                          {[...Array(data?.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+
                 <ListGroup.Item>
                   <Button
+                    onClick={addToCart}
                     className="btn btn-block"
                     type="button"
                     disabled={data?.countInStock === 0}
