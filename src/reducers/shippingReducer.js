86832322ad046a822614d7ed42d099f861096d2e
Ramez Taher <ramezgamer11@gmail.com>
2022-10-11 21:42:38 +0100

    save shipping address reducer

---
+++ src/reducers/shippingReducer.js
@@ -0,0 +1,14 @@
+import { SHIPPING_SAVE_ADDRESS } from "../constants/shippingsConstants"
+
+export const shippingReducer = (state = {}, action) => {
+  switch (action.type) {
+    case SHIPPING_SAVE_ADDRESS:
+      return {
+        ...state,
+        shippingAddress: action.payload,
+      }
+
+    default:
+      return state
+  }
+}
