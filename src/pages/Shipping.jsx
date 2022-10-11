8ef166ce68378e80c1ea46138f05815c93557bdb
Ramez Taher <ramezgamer11@gmail.com>
2022-10-11 21:20:31 +0100

    building the shipping form

--- src/pages/Shipping.jsx
+++ src/pages/Shipping.jsx
@@ -1,21 +1,57 @@
 import React, { useState } from "react"
 import { Form } from "react-bootstrap"
-import  FormWrapper  from "../components/FormWrapper"
+import FormWrapper from "../components/FormWrapper"
 
 const Shipping = () => {
   const [address, setAddress] = useState("")
   const [city, setCity] = useState("")
-  const [zip, setZip] = useState("")
+  const [zipCode, setZipCode] = useState("")
   const [country, setCountry] = useState("")
 
-  const handleSubmit=()=>{
-
-  }
+  const handleSubmit = () => {}
   return (
     <>
       <FormWrapper>
         <h1>Shipping</h1>
-        <Form onSubmit={handleSubmit}></Form>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="address" className="mb-2">
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter address"
+              value={address}
+              required
+              onChange={(e) => setAddress(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="city" className="mb-2">
+            <Form.Label>City</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter city"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="zip" className="mb-2">
+            <Form.Label>ZipCode</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter The Zip Code"
+              value={zipCode}
+              onChange={(e) => setZipCode(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId="zip" className="mb-2">
+            <Form.Label>Country</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Country"
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+        </Form>
       </FormWrapper>
     </>
   )
