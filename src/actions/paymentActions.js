d272564ddf2c90683f4a3f59aac1b0c68a0d8629
Ramez Taher <ramezgamer11@gmail.com>
2022-10-12 22:50:45 +0100

    payment actions done

---
+++ src/actions/paymentActions.js
@@ -0,0 +1,10 @@
+import { PAYMENT_SAVE_METHOD } from "../constants/paymentConstants"
+
+export const savePaymentMethod = (method) => (dispatch) => {
+  dispatch({
+    type: PAYMENT_SAVE_METHOD,
+    payload: method,
+  })
+
+  localStorage.setItem("paymentMethod", JSON.stringify(method))
+}
