c1e5a0b6f5f0ba15892570df0fcab54b40eb15d5
Ramez Taher <ramezgamer11@gmail.com>
2022-10-12 22:21:59 +0100

    steps compoenents done

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React from "react"
+import { Nav } from "react-bootstrap"
+import { LinkContainer } from "react-router-bootstrap"
 
-const Steps = () => {
+const Steps = ({ step1, step2, step3, step4 }) => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Nav className="justify-content-center mb-4">
+        <Nav.Item>
+          {step1 ? (
+            <LinkContainer to="/login">
+              <Nav.Link>Sign In</Nav.Link>
+            </LinkContainer>
+          ) : (
+            <Nav.Link disabled>Sign In</Nav.Link>
+          )}
+        </Nav.Item>
+        <Nav.Item>
+          {step2 ? (
+            <LinkContainer to="/shipping">
+              <Nav.Link>Shipping</Nav.Link>
+            </LinkContainer>
+          ) : (
+            <Nav.Link disabled>Shipping</Nav.Link>
+          )}
+        </Nav.Item>
+        <Nav.Item>
+          {step3 ? (
+            <LinkContainer to="/payment">
+              <Nav.Link>Payment</Nav.Link>
+            </LinkContainer>
+          ) : (
+            <Nav.Link disabled>Payment</Nav.Link>
+          )}
+        </Nav.Item>
+        <Nav.Item>
+          {step4 ? (
+            <LinkContainer to="/order">
+              <Nav.Link>Review & Order</Nav.Link>
+            </LinkContainer>
+          ) : (
+            <Nav.Link disabled>Review & Order</Nav.Link>
+          )}
+        </Nav.Item>
+      </Nav>
+    </>
   )
 }
 
