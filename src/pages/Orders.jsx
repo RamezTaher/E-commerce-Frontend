92c0c847d4c57737b654ff04df2c39c4babdc692
Ramez Taher <ramezgamer11@gmail.com>
2022-10-14 20:59:36 +0100

    bringing items information from the redux state

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,11 +1,15 @@
 import React from "react"
-import { Col, ListGroup, Row } from "react-bootstrap"
+import { Col, Image, ListGroup, Row } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+import Message from "../components/Message"
 import Steps from "../components/Steps"
 
 const Orders = () => {
   const dispatch = useDispatch()
   const shipping = useSelector((state) => state.shippingAddress)
+  const payment = useSelector((state) => state.payment)
+  const cart = useSelector((state) => state.cart)
   return (
     <>
       <Steps step1 step2 step3 step4 />
@@ -19,11 +23,43 @@ const Orders = () => {
                 {shipping.country}
               </p>
             </ListGroup.Item>
+
             <ListGroup.Item>
               <h2>Payment</h2>
-              <p>
-                
-              </p>
+              <p>Method: {payment.payementMethod}</p>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Orders</h2>
+              {cart.cartItems.length === 0 ? (
+                <Message>Add Items to your Cart</Message>
+              ) : (
+                <ListGroup variant="flush">
+                  {cart.cartItems.map((item, idx) => (
+                    <ListGroup.Item key={idx}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                        <Col>
+                          <Link to={`/products/${item.product}`}>
+                            {item.name}
+                          </Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.quantity} x ${item.price} = $
+                          {(item.quantity * item.price).toFixed(2)}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
             </ListGroup.Item>
           </ListGroup>
         </Col>
