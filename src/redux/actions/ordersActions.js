d8586af2d8f648e5c73aed0841ad5bdd57cd2c3d
Ramez Taher <ramezgamer11@gmail.com>
2022-10-14 21:53:10 +0100

    finishing the redux thing with orders

---
+++ src/redux/actions/ordersActions.js
@@ -0,0 +1,45 @@
+import axios from "axios"
+import { API_URL } from "../../constants/api"
+import {
+  ORDERS_REQUEST,
+  ORDERS_SUCCESS,
+  ORDERS_FAIL,
+} from "../../constants/orderConstants"
+
+export const createOrder = (order) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDERS_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo?.token}`,
+      },
+    }
+
+    const { data } = await axios.post(
+      `${API_URL}/api/users/profile`,
+      order,
+      config
+    )
+
+    dispatch({
+      type: ORDERS_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: ORDERS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
