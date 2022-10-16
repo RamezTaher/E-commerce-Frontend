17dec675983360c37903f4f79e913f01d399c5cc
Ramez Taher <ramezgamer11@gmail.com>
2022-10-16 01:34:37 +0100

    update the order paiment status action

--- src/redux/actions/ordersActions.js
+++ src/redux/actions/ordersActions.js
@@ -1,76 +1,103 @@
-import axios from "axios"
-import { API_URL } from "../../constants/api"
+import axios from "axios";
+import { API_URL } from "../../constants/api";
 import {
-  POST_ORDER_SUCCESS,
-  POST_ORDER_REQUEST,
-  POST_ORDER_FAIL,
-  GET_ORDER_SUCCESS,
-  GET_ORDER_FAIL,
-  GET_ORDER_REQUEST,
-} from "../../constants/orderConstants"
+    POST_ORDER_SUCCESS,
+    POST_ORDER_REQUEST,
+    POST_ORDER_FAIL,
+    GET_ORDER_SUCCESS,
+    GET_ORDER_FAIL,
+    GET_ORDER_REQUEST,
+    PUT_ORDER_REQUEST,
+    PUT_ORDER_SUCCESS,
+    PUT_ORDER_FAIL,
+} from "../../constants/orderConstants";
 
-export const createOrder = (order) => async (dispatch, getState) => {
-  try {
-    dispatch({
-      type: POST_ORDER_REQUEST,
-    })
+export const createOrder = order => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: POST_ORDER_REQUEST,
+        });
 
-    const {
-      userLogin: { userInfo },
-    } = getState()
+        const {
+            userLogin: { userInfo },
+        } = getState();
 
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${userInfo?.token}`,
-      },
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${userInfo?.token}`,
+            },
+        };
+
+        const { data } = await axios.post(`${API_URL}/api/orders`, order, config);
+
+        dispatch({
+            type: POST_ORDER_SUCCESS,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: POST_ORDER_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+        });
     }
+};
+export const getOrder = id => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: GET_ORDER_REQUEST,
+        });
 
-    const { data } = await axios.post(`${API_URL}/api/orders`, order, config)
+        const {
+            userLogin: { userInfo },
+        } = getState();
 
-    dispatch({
-      type: POST_ORDER_SUCCESS,
-      payload: data,
-    })
-  } catch (error) {
-    dispatch({
-      type: POST_ORDER_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    })
-  }
-}
-export const getOrder = (id) => async (dispatch, getState) => {
-  try {
-    dispatch({
-      type: GET_ORDER_REQUEST,
-    })
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo?.token}`,
+            },
+        };
 
-    const {
-      userLogin: { userInfo },
-    } = getState()
+        const { data } = await axios.get(`${API_URL}/api/orders/${id}`, config);
 
-    const config = {
-      headers: {
-        Authorization: `Bearer ${userInfo?.token}`,
-      },
+        dispatch({
+            type: GET_ORDER_SUCCESS,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: GET_ORDER_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+        });
     }
+};
+export const putOrder = (id, paidRes) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: PUT_ORDER_REQUEST,
+        });
 
-    const { data } = await axios.get(`${API_URL}/api/orders/${id}`, config)
+        const {
+            userLogin: { userInfo },
+        } = getState();
 
-    dispatch({
-      type: GET_ORDER_SUCCESS,
-      payload: data,
-    })
-  } catch (error) {
-    dispatch({
-      type: GET_ORDER_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    })
-  }
-}
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${userInfo?.token}`,
+            },
+        };
+
+        const { data } = await axios.put(`${API_URL}/api/orders/${id}/pay`, paidRes, config);
+
+        dispatch({
+            type: PUT_ORDER_SUCCESS,
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: PUT_ORDER_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+        });
+    }
+};
