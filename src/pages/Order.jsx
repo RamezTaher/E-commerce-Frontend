f9315f911f88f67a6940752c2b24cd7bca0021bd
Ramez Taher <ramezgamer11@gmail.com>
2022-10-16 01:12:55 +0100

    adding some user information to the order page

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,124 +1,117 @@
-import React, { useEffect } from "react"
-import { Card, Col, Image, ListGroup, Row } from "react-bootstrap"
-import { useDispatch, useSelector } from "react-redux"
-import { useParams } from "react-router-dom"
-import { Link, useNavigate } from "react-router-dom"
-import Loader from "../components/Loader"
-import Message from "../components/Message"
-import { getOrder } from "../redux/actions/ordersActions"
-import { removeWhiteSpaces } from "../utils/removeWhiteSpaces"
+import React, { useEffect } from "react";
+import { Card, Col, Image, ListGroup, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { getOrder } from "../redux/actions/ordersActions";
+import { removeWhiteSpaces } from "../utils/removeWhiteSpaces";
 
 const Order = () => {
-  const dispatch = useDispatch()
-  let { id } = useParams()
+    const dispatch = useDispatch();
+    let { id } = useParams();
 
-  const getOrderData = useSelector((state) => state.getOrder)
+    const getOrderData = useSelector(state => state.getOrder);
 
-  const { data, loading, error } = getOrderData
-  console.log(data)
+    const { data, loading, error } = getOrderData;
+    console.log(data);
 
-  useEffect(() => {
-    dispatch(getOrder(id))
-  }, [dispatch, id])
+    useEffect(() => {
+        dispatch(getOrder(id));
+    }, [dispatch, id]);
 
-  return loading ? (
-    <Loader />
-  ) : error ? (
-    <Message variant={"danger"}>{error}</Message>
-  ) : (
-    <>
-      <h1>Order {data?._id}</h1>
-      <Row>
-        <Col md={8}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h2>Shipping</h2>
-              <p>
-                Address: {removeWhiteSpaces(data.shippingAddress.address)},{" "}
-                {removeWhiteSpaces(data.shippingAddress.city)},{" "}
-                {removeWhiteSpaces(data.shippingAddress.zipCode)},{" "}
-                {removeWhiteSpaces(data.shippingAddress.country)}
-              </p>
-            </ListGroup.Item>
+    return loading ? (
+        <Loader />
+    ) : error ? (
+        <Message variant={"danger"}>{error}</Message>
+    ) : (
+        <>
+            <h1>Order {data?._id}</h1>
+            <Row>
+                <Col md={8}>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>
+                            <h2>Shipping</h2>
+                            <p style={{ fontWeight: "bold" }}>{data?.user?.name} </p>
+                            <p>
+                                <span style={{ fontWeight: "bold" }}>Address:</span>
+                                {removeWhiteSpaces(data.shippingAddress.address)},{" "}
+                                {removeWhiteSpaces(data.shippingAddress.city)},{" "}
+                                {removeWhiteSpaces(data.shippingAddress.zipCode)},{" "}
+                                {removeWhiteSpaces(data.shippingAddress.country)}
+                            </p>
+                        </ListGroup.Item>
 
-            <ListGroup.Item>
-              <h2>Payment</h2>
-              <p>Method: {data.paymentMethod}</p>
-            </ListGroup.Item>
+                        <ListGroup.Item>
+                            <h2>Payment</h2>
+                            <p>Method: {data.paymentMethod}</p>
+                        </ListGroup.Item>
 
-            <ListGroup.Item>
-              <h2>Items Ordered</h2>
-              {data.orderItems.length === 0 ? (
-                <Message>Empty Order</Message>
-              ) : (
-                <ListGroup variant="flush">
-                  {data?.orderItems.map((item, idx) => (
-                    <ListGroup.Item key={idx}>
-                      <Row>
-                        <Col md={2}>
-                          <Image
-                            src={item.image}
-                            alt={item.name}
-                            fluid
-                            rounded
-                          />
-                        </Col>
-                        <Col>
-                          <Link to={`/products/${item.product}`}>
-                            {item.name}
-                          </Link>
-                        </Col>
-                        <Col md={4}>
-                          {item.quantity} x ${item.price} = $
-                          {(item?.quantity * item?.price).toFixed(2)}
-                        </Col>
-                      </Row>
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              )}
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={4}>
-          <Card>
-            <ListGroup variant="flush">
-              <ListGroup.Item>
-                <h2>Orders</h2>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Items</Col>
-                  <Col>${data.itemsPrice}</Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Shipping</Col>
-                  <Col>${data.shippingPrice}</Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Tax</Col>
-                  <Col>${data.taxPrice}</Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>Total</Col>
-                  <Col>${data.totalPrice}</Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                {error && <Message variant={"danger"}>{error}</Message>}
-              </ListGroup.Item>
-            </ListGroup>
-          </Card>
-        </Col>
-      </Row>
-    </>
-  )
-}
+                        <ListGroup.Item>
+                            <h2>Items Ordered</h2>
+                            {data.orderItems.length === 0 ? (
+                                <Message>Empty Order</Message>
+                            ) : (
+                                <ListGroup variant="flush">
+                                    {data?.orderItems.map((item, idx) => (
+                                        <ListGroup.Item key={idx}>
+                                            <Row>
+                                                <Col md={2}>
+                                                    <Image src={item.image} alt={item.name} fluid rounded />
+                                                </Col>
+                                                <Col>
+                                                    <Link to={`/products/${item.product}`}>{item.name}</Link>
+                                                </Col>
+                                                <Col md={4}>
+                                                    {item.quantity} x ${item.price} = $
+                                                    {(item?.quantity * item?.price).toFixed(2)}
+                                                </Col>
+                                            </Row>
+                                        </ListGroup.Item>
+                                    ))}
+                                </ListGroup>
+                            )}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={4}>
+                    <Card>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                <h2>Orders</h2>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Items</Col>
+                                    <Col>${data.itemsPrice}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Shipping</Col>
+                                    <Col>${data.shippingPrice}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Tax</Col>
+                                    <Col>${data.taxPrice}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Total</Col>
+                                    <Col>${data.totalPrice}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>{error && <Message variant={"danger"}>{error}</Message>}</ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </>
+    );
+};
 
-export default Order
+export default Order;
