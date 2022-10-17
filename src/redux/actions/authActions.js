5bbefa0ef41272c731ea50ce5e1f9b83ef0125a5
Ramez Taher <ramezgamer11@gmail.com>
2022-10-17 02:40:16 +0100

    bug fixed

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -1,94 +1,88 @@
-import axios from "axios"
-import { API_URL } from "../../constants/api"
+import axios from "axios";
+import { API_URL } from "../../constants/api";
 import {
-  LOGIN_FAIL,
-  LOGIN_LOGOUT,
-  LOGIN_REQUEST,
-  LOGIN_SUCCESS,
-  REGISTER_FAIL,
-  REGISTER_REQUEST,
-  REGISTER_SUCCESS,
-} from "../../constants/authConstants"
+    LOGIN_FAIL,
+    LOGIN_LOGOUT,
+    LOGIN_REQUEST,
+    LOGIN_SUCCESS,
+    REGISTER_FAIL,
+    REGISTER_REQUEST,
+    REGISTER_SUCCESS,
+} from "../../constants/authConstants";
+import { RESET_ALLORDERS } from "../../constants/orderConstants";
+import { PROFILE_RESET } from "../../constants/profileConstants";
 
-export const login = (email, password) => async (dispatch) => {
-  try {
-    dispatch({
-      type: LOGIN_REQUEST,
-    })
+export const login = (email, password) => async dispatch => {
+    try {
+        dispatch({
+            type: LOGIN_REQUEST,
+        });
 
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-      },
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+
+        const { data } = await axios.post(
+            `${API_URL}/api/users/login`,
+            {
+                email,
+                password,
+            },
+            config,
+        );
+
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: data,
+        });
+
+        localStorage.setItem("userInfo", JSON.stringify(data));
+    } catch (error) {
+        dispatch({
+            type: LOGIN_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+        });
     }
+};
 
-    const { data } = await axios.post(
-      `${API_URL}/api/users/login`,
-      {
-        email,
-        password,
-      },
-      config
-    )
+export const register = (name, email, password) => async dispatch => {
+    try {
+        dispatch({
+            type: REGISTER_REQUEST,
+        });
 
-    dispatch({
-      type: LOGIN_SUCCESS,
-      payload: data,
-    })
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
 
-    localStorage.setItem("userInfo", JSON.stringify(data))
-  } catch (error) {
-    dispatch({
-      type: LOGIN_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    })
-  }
-}
+        const { data } = await axios.post(`${API_URL}/api/users`, { name, email, password }, config);
 
-export const register = (name, email, password) => async (dispatch) => {
-  try {
-    dispatch({
-      type: REGISTER_REQUEST,
-    })
+        dispatch({
+            type: REGISTER_SUCCESS,
+            payload: data,
+        });
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: data,
+        });
 
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-      },
+        localStorage.setItem("userInfo", JSON.stringify(data));
+    } catch (error) {
+        dispatch({
+            type: REGISTER_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+        });
     }
+};
 
-    const { data } = await axios.post(
-      `${API_URL}/api/users`,
-      { name, email, password },
-      config
-    )
-
-    dispatch({
-      type: REGISTER_SUCCESS,
-      payload: data,
-    })
-    dispatch({
-      type: LOGIN_SUCCESS,
-      payload: data,
-    })
-
-    localStorage.setItem("userInfo", JSON.stringify(data))
-  } catch (error) {
-    dispatch({
-      type: REGISTER_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    })
-  }
-}
-
-export const logout = () => (dispatch) => {
-  localStorage.removeItem("userInfo")
-  localStorage.removeItem("shippingAddress")
-  dispatch({ type: LOGIN_LOGOUT })
-}
+export const logout = () => dispatch => {
+    localStorage.removeItem("userInfo");
+    localStorage.removeItem("shippingAddress");
+    dispatch({ type: LOGIN_LOGOUT });
+    dispatch({ type: PROFILE_RESET });
+    dispatch({ type: RESET_ALLORDERS });
+};
