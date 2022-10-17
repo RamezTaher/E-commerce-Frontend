02c167e3be56975f10396f7dee8644fb13e3db42
Ramez Taher <ramezgamer11@gmail.com>
2022-10-17 02:37:47 +0100

    profile reset reducer

--- src/redux/reducers/profileReducer.js
+++ src/redux/reducers/profileReducer.js
@@ -1,36 +1,38 @@
 import {
-  PROFILE_FAIL,
-  PROFILE_REQUEST,
-  PROFILE_SUCCESS,
-  PROFILE_UPDATE_FAIL,
-  PROFILE_UPDATE_REQUEST,
-  PROFILE_UPDATE_SUCCESS,
-} from "../../constants/profileConstants"
+    PROFILE_FAIL,
+    PROFILE_REQUEST,
+    PROFILE_RESET,
+    PROFILE_SUCCESS,
+    PROFILE_UPDATE_FAIL,
+    PROFILE_UPDATE_REQUEST,
+    PROFILE_UPDATE_SUCCESS,
+} from "../../constants/profileConstants";
 
 export const profileReducer = (state = { user: {} }, action) => {
-  switch (action.type) {
-    case PROFILE_REQUEST:
-      return { ...state, loading: true }
-    case PROFILE_SUCCESS:
-      return { loading: false, user: action.payload }
-    case PROFILE_FAIL:
-      return { loading: false, error: action.payload }
-
-    default:
-      return state
-  }
-}
+    switch (action.type) {
+        case PROFILE_REQUEST:
+            return { ...state, loading: true };
+        case PROFILE_SUCCESS:
+            return { loading: false, user: action.payload };
+        case PROFILE_FAIL:
+            return { loading: false, error: action.payload };
+        case PROFILE_RESET:
+            return {};
+        default:
+            return state;
+    }
+};
 
 export const profileUpdateReducer = (state = {}, action) => {
-  switch (action.type) {
-    case PROFILE_UPDATE_REQUEST:
-      return { loading: true }
-    case PROFILE_UPDATE_SUCCESS:
-      return { loading: false, success: true, userInfo: action.payload }
-    case PROFILE_UPDATE_FAIL:
-      return { loading: false, error: action.payload }
+    switch (action.type) {
+        case PROFILE_UPDATE_REQUEST:
+            return { loading: true };
+        case PROFILE_UPDATE_SUCCESS:
+            return { loading: false, success: true, userInfo: action.payload };
+        case PROFILE_UPDATE_FAIL:
+            return { loading: false, error: action.payload };
 
-    default:
-      return state
-  }
-}
+        default:
+            return state;
+    }
+};
