23f89c1048d7fb621558bfc5612a51d95aaeec44
Ramez Taher <ramezgamer11@gmail.com>
2022-10-17 19:12:43 +0100

    users reducer done

--- src/redux/reducers/usersReducer.js
+++ src/redux/reducers/usersReducer.js
@@ -1,14 +1,13 @@
 import { USERS_FAIL, USERS_REQUEST, USERS_SUCCESS } from "../../constants/users";
 
-export const usersReducer = (state = { user: {} }, action) => {
+export const usersReducer = (state = { users: [] }, action) => {
     switch (action.type) {
         case USERS_REQUEST:
             return { ...state, loading: true };
         case USERS_SUCCESS:
-            return { loading: false, user: action.payload };
+            return { loading: false, users: action.payload };
         case USERS_FAIL:
             return { loading: false, error: action.payload };
-
         default:
             return state;
     }
