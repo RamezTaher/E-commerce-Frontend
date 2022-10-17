50f41c7237c734e4aee9bfc839eea43904d967dc
Ramez Taher <ramezgamer11@gmail.com>
2022-10-17 01:52:12 +0100

    import the reducer in the store of redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,58 +1,53 @@
-import { createStore, combineReducers, applyMiddleware } from "redux"
-import thunk from "redux-thunk"
-import { composeWithDevTools } from "redux-devtools-extension"
-import { productsReducer } from "./reducers/productsReducer"
-import { productReducer } from "./reducers/productReducer"
-import { cartReducer } from "./reducers/cartReducer"
-import { loginReducer, registerReducer } from "./reducers/authReducer"
-import { profileReducer, profileUpdateReducer } from "./reducers/profileReducer"
-import { shippingReducer } from "./reducers/shippingReducer"
-import { paymentReducer } from "./reducers/paymentReducer"
-import { createOrderReducer, getOrderReducer, putOrderReducer } from "./reducers/ordersReducer"
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { productsReducer } from "./reducers/productsReducer";
+import { productReducer } from "./reducers/productReducer";
+import { cartReducer } from "./reducers/cartReducer";
+import { loginReducer, registerReducer } from "./reducers/authReducer";
+import { profileReducer, profileUpdateReducer } from "./reducers/profileReducer";
+import { shippingReducer } from "./reducers/shippingReducer";
+import { paymentReducer } from "./reducers/paymentReducer";
+import { createOrderReducer, getAllOrdersReducer, getOrderReducer, putOrderReducer } from "./reducers/ordersReducer";
 
 const reducer = combineReducers({
-  productsList: productsReducer,
-  productDetails: productReducer,
-  cart: cartReducer,
-  userLogin: loginReducer,
-  userRegister: registerReducer,
-  profile: profileReducer,
-  profileUpdate: profileUpdateReducer,
-  shippingAddress: shippingReducer,
-  payment: paymentReducer,
-  createOrder: createOrderReducer,
-  getOrder: getOrderReducer,
-  putOrder: putOrderReducer,
-})
+    productsList: productsReducer,
+    productDetails: productReducer,
+    cart: cartReducer,
+    userLogin: loginReducer,
+    userRegister: registerReducer,
+    profile: profileReducer,
+    profileUpdate: profileUpdateReducer,
+    shippingAddress: shippingReducer,
+    payment: paymentReducer,
+    createOrder: createOrderReducer,
+    getOrder: getOrderReducer,
+    putOrder: putOrderReducer,
+    getAllOrders: getAllOrdersReducer,
+});
 
 const cartItemsFromLocalStorage = localStorage.getItem("cartItems")
-  ? JSON.parse(localStorage.getItem("cartItems"))
-  : []
+    ? JSON.parse(localStorage.getItem("cartItems"))
+    : [];
 
-const userInfoFromLocalStorage = localStorage.getItem("userInfo")
-  ? JSON.parse(localStorage.getItem("userInfo"))
-  : null
+const userInfoFromLocalStorage = localStorage.getItem("userInfo") ? JSON.parse(localStorage.getItem("userInfo")) : null;
 
 const shippingAddressFromLocalStorage = localStorage.getItem("shippingAddress")
-  ? JSON.parse(localStorage.getItem("shippingAddress"))
-  : {}
+    ? JSON.parse(localStorage.getItem("shippingAddress"))
+    : {};
 const paymentMethodFromLocalStorage = localStorage.getItem("paymentMethod")
-  ? JSON.parse(localStorage.getItem("paymentMethod"))
-  : null
+    ? JSON.parse(localStorage.getItem("paymentMethod"))
+    : null;
 
 const initialState = {
-  cart: { cartItems: cartItemsFromLocalStorage },
-  userLogin: { userInfo: userInfoFromLocalStorage },
-  shippingAddress: shippingAddressFromLocalStorage,
-  payment: { payementMethod: paymentMethodFromLocalStorage },
-}
+    cart: { cartItems: cartItemsFromLocalStorage },
+    userLogin: { userInfo: userInfoFromLocalStorage },
+    shippingAddress: shippingAddressFromLocalStorage,
+    payment: { payementMethod: paymentMethodFromLocalStorage },
+};
 
-const middleware = [thunk]
+const middleware = [thunk];
 
-const store = createStore(
-  reducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-)
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
 
-export default store
+export default store;
