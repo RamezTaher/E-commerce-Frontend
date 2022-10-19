e16819a142ce5f19f499e589c3b6b033a2401946
Ramez Taher <ramezgamer11@gmail.com>
2022-10-19 21:03:56 +0100

    done for the delete product reducer

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,21 +1,33 @@
 import {
-  PRODUCT_FAIL,
-  PRODUCT_SUCCESS,
-  PRODUCT_REQUEST,
-} from "../../constants/productConstants"
+    PRODUCT_FAIL,
+    PRODUCT_SUCCESS,
+    PRODUCT_REQUEST,
+    DELETE_PRODUCT_REQUEST,
+    DELETE_PRODUCT_SUCCESS,
+    DELETE_PRODUCT_FAIL,
+} from "../../constants/productConstants";
 
-export const productReducer = (
-  state = { product: { reviews: [] } },
-  action
-) => {
-  switch (action.type) {
-    case PRODUCT_REQUEST:
-      return { loading: true, data: [] }
-    case PRODUCT_SUCCESS:
-      return { loading: false, data: action.payload }
-    case PRODUCT_FAIL:
-      return { loading: false, error: action.payload }
-    default:
-      return state
-  }
-}
+export const productReducer = (state = { product: { reviews: [] } }, action) => {
+    switch (action.type) {
+        case PRODUCT_REQUEST:
+            return { loading: true, data: [] };
+        case PRODUCT_SUCCESS:
+            return { loading: false, data: action.payload };
+        case PRODUCT_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
+export const deleteProductReducer = (state = { success: false }, action) => {
+    switch (action.type) {
+        case DELETE_PRODUCT_REQUEST:
+            return { loading: true, success: false };
+        case DELETE_PRODUCT_SUCCESS:
+            return { loading: false, success: true };
+        case DELETE_PRODUCT_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
