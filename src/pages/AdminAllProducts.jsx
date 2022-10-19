fb12a5d3a76dfdd2d0f26231f6e982cdcd2027ae
Ramez Taher <ramezgamer11@gmail.com>
2022-10-19 20:43:48 +0100

    Products page

---
+++ src/pages/AdminAllProducts.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { useEffect } from "react";
+import { Button, Col, Row, Table } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from "react-router-bootstrap";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { products } from "../redux/actions/productsActions";
+
+const AdminAllProducts = () => {
+    const dispatch = useDispatch();
+    const productsData = useSelector(state => state.productsList);
+    const { loading, error, users } = productsData;
+
+    useEffect(() => {
+        dispatch(products());
+    }, [dispatch]);
+
+    console.log(users);
+
+    const deleteHandler = id => {
+        // need to add a custom confirmation later on
+        if (window.confirm("Are you sure to delete this user")) {
+        }
+    };
+
+    const createNewProductHandler = () => {};
+    return (
+        <>
+            <Row className="align-items-center">
+                <Col>
+                    <h1>Products</h1>
+                </Col>
+                <Col className="text-right">
+                    <Button className="my-3" onClick={createNewProductHandler}>
+                        <i className="fas fa-plus"></i>
+                        Create New Product{" "}
+                    </Button>
+                </Col>
+            </Row>
+            {loading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant="danger">{error}</Message>
+            ) : (
+                <Table striped bordered hover responsive className="table-sm">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>NAME</th>
+                            <th>PRICE</th>
+                            <th>CATEGORY</th>
+                            <th>BRAND</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products.map((product, idx) => (
+                            <tr key={idx}>
+                                <td>{product?._id}</td>
+                                <td>{product?.name}</td>
+                                <td>${product?.price}</td>
+                                <td>{product?.category}</td>
+                                <td>{product?.brand}</td>
+                                <td>
+                                    <LinkContainer to={`/admin/products/${product?._id}/modify`}>
+                                        <Button variant="light" className="btn-sm">
+                                            <i className="fas fa-edit"></i>
+                                        </Button>
+                                    </LinkContainer>
+                                    <Button
+                                        variant="danger"
+                                        className="btn-sm"
+                                        onClick={() => deleteHandler(product?._id)}
+                                    >
+                                        <i className="fas fa-trash"></i>
+                                    </Button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            )}
+        </>
+    );
+};
+
+export default AdminAllProducts;
