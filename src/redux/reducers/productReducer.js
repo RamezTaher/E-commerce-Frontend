9495da13d572ecf50841ea1b52af7c5f96d11f91
Ramez Taher <ramezgamer11@gmail.com>
2022-10-20 21:09:12 +0100

    post product reducer

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -5,6 +5,10 @@ import {
     DELETE_PRODUCT_REQUEST,
     DELETE_PRODUCT_SUCCESS,
     DELETE_PRODUCT_FAIL,
+    POST_PRODUCT_REQUEST,
+    POST_PRODUCT_SUCCESS,
+    POST_PRODUCT_FAIL,
+    POST_PRODUCT_RESET,
 } from "../../constants/productConstants";
 
 export const productReducer = (state = { product: { reviews: [] } }, action) => {
@@ -27,6 +31,21 @@ export const deleteProductReducer = (state = { success: false }, action) => {
             return { loading: false, success: true };
         case DELETE_PRODUCT_FAIL:
             return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
+
+export const postProductReducer = (state = {}, action) => {
+    switch (action.type) {
+        case POST_PRODUCT_REQUEST:
+            return { loading: true };
+        case POST_PRODUCT_SUCCESS:
+            return { loading: false, success: true, product: action.payload };
+        case POST_PRODUCT_FAIL:
+            return { loading: false, error: action.payload };
+        case POST_PRODUCT_RESET:
+            return {};
         default:
             return state;
     }
