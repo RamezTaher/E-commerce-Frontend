45a22376ee08c031aa1cf5bc42119c8baf434334
Ramez Taher <ramezgamer11@gmail.com>
2022-10-23 00:17:10 +0100

    done with vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-refresh";
 
 export default defineConfig({
     plugins: [react()],
