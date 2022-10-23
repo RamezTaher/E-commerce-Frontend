c7f9730d8df8a8e46bb8635e23113629e705810a
Ramez Taher <ramezgamer11@gmail.com>
2022-10-23 01:01:42 +0100

    configure vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-refresh";
 
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(configEnv => ({
     plugins: [react()],
-});
+    build: {
+        outDir: "build",
+    },
+}));
