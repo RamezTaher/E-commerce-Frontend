a4a68093ab1bcaf0f1306c1cd50cca64e1d4467e
Ramez Taher <ramezgamer11@gmail.com>
2022-11-23 12:10:37 +0100

    done with the search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Form, Placeholder } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const Search = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const [keyword, setKeyword] = useState("");
+    const navigate = useNavigate();
 
-export default Search
+    const onSubmitHandler = e => {
+        e.preventDefault();
+        if (keyword.trim()) {
+            navigate(`/search/${keyword}`);
+        } else {
+            navigate("/");
+        }
+    };
+    return (
+        <>
+            <Form onSubmit={onSubmitHandler}>
+                <Form.Control
+                    type="text"
+                    name="q"
+                    onChange={e => setKeyword(e.target.value)}
+                    placeholder="Search Products..."
+                    className="mr-sm-2 ml-sm-5"
+                ></Form.Control>
+                <Button type="submit" variant="outline-success" className="p-2">
+                    {" "}
+                    Search{" "}
+                </Button>
+            </Form>
+        </>
+    );
+};
+
+export default Search;
